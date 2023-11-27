3ec7487682d7f584f48a93db856742e943177ca1
Cloud Chef Labs <admin@cloudchef-labs.com>
2023-11-27 15:56:19 +0900

    configuration template added.

---
+++ src/main/java/co/cloudcheflabs/chango/log/Chango.java
@@ -0,0 +1,39 @@
+package co.cloudcheflabs.chango.log;
+
+import co.cloudcheflabs.chango.log.component.TestComp;
+import co.cloudcheflabs.chango.log.config.ConfigurationLoader;
+import co.cloudcheflabs.chango.log.util.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Chango {
+
+    private static Logger LOG = LoggerFactory.getLogger(Chango.class);
+
+    public static final String ENV_CHANGO_LOG_CONFIGURATION_PATH = "CHANGO_LOG_CONFIGURATION_PATH";
+
+    public static void main(String[] args) {
+        if(args.length < 1) {
+            throw new RuntimeException("Configuration path not available!");
+        }
+
+        String confPath = args[0];
+
+        StringUtils.setEnv(ENV_CHANGO_LOG_CONFIGURATION_PATH, confPath);
+        LOG.info("env {}: {}", ENV_CHANGO_LOG_CONFIGURATION_PATH,
+                StringUtils.getEnv(ENV_CHANGO_LOG_CONFIGURATION_PATH));
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(
+                ConfigurationLoader.class,
+                TestComp.class
+        );
+
+        try {
+            Thread.sleep(Long.MAX_VALUE);
+        } catch (Exception e) {
+            LOG.error(e.getMessage());
+        }
+    }
+}
---
+++ src/test/java/co/cloudcheflabs/chango/log/RunChangoLogTestRunner.java
@@ -0,0 +1,19 @@
+package co.cloudcheflabs.chango.log;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class RunChangoLogTestRunner {
+
+    @Test
+    public void runChangoLog() throws Exception {
+
+        String confPath = System.getProperty("confPath", "/Users/mykidong/project/chango-log/src/main/resources/configuration.yml");
+
+        List<String> args = Arrays.asList(confPath);
+
+        Chango.main(args.toArray(new String[0]));
+    }
+}
